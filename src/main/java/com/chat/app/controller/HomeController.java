9a029b2333719cbb4b9636c7e99f69bd227ac781
El santi <95284926+santibarriento@users.noreply.github.com>
2025-08-27 18:20:52 +0200

    Create HomeController.java

---
+++ src/main/java/com/chat/app/controller/HomeController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Real-Time Chat Spring Boot is running!";
+    }
+}
